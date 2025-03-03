2d554036efe8a68426466a7dae96f786bcd6a6a0
fsavevsk <filip.savevski@netcetera.com>
2025-03-03 22:01:57 +0100

    Initial Commit

---
+++ src/test/java/com/fsavevsk/timetrackingapp/TimeTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsavevsk.timetrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
