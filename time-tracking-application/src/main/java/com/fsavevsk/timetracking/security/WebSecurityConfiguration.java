26cf6407d381120b54970d11f80fa8ccf6493aca
fsavevsk <filip.savevski@netcetera.com>
2025-11-09 21:06:10 +0100

    Enhance CI workflow with security scans and update CSRF configuration

--- time-tracking-application/src/main/java/com/fsavevsk/timetracking/security/WebSecurityConfiguration.java
+++ time-tracking-application/src/main/java/com/fsavevsk/timetracking/security/WebSecurityConfiguration.java
@@ -25,7 +25,7 @@ public class WebSecurityConfiguration {
                 )
                 .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
                 .cors(withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new CustomAuthenticationConverter())))
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));;
